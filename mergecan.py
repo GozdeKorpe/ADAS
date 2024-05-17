97ed92def1a77aece200a0a165efefbce79dd1ae
GozdeKorpe <78368128+GozdeKorpe@users.noreply.github.com>
2024-05-17 15:09:50 +0300

    Update mergecan.py

--- mergecan.py
+++ mergecan.py
@@ -274,8 +274,6 @@ class DrowsinessDetector:
 
 def main():
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     tracker = handTracker()
     wheel_detector = WheelDetector()
     eye_detector = DrowsinessDetector()
